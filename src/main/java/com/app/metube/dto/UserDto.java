e70d6f8a2e581848d927645758c833cf0350ac4d
RaresCristescu <rares.cristescu@yahoo.com>
2025-12-19 16:19:37 +0200

    added lombok

--- src/main/java/com/app/metube/dto/UserDto.java
+++ src/main/java/com/app/metube/dto/UserDto.java
@@ -1,39 +1,14 @@
 package com.app.metube.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class UserDto {
 	private String login;
 	private String email;
 	private String password;
-	
-	public UserDto(String login, String email, String password) {
-		super();
-		this.login = login;
-		this.email = email;
-		this.password = password;
-	}
-
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
 }
