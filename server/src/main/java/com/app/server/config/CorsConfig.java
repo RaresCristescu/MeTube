38b7e912351932d5add808ba6c83c743ae3cbbc7
RaresCristescu <rares.cristescu123@gmail.com>
2026-01-10 12:23:43 +0200

    moved cors config to its own confgi file

--- server/src/main/java/com/app/server/config/CorsConfig.java
+++ server/src/main/java/com/app/server/config/CorsConfig.java
@@ -1,23 +1,30 @@
-//package com.app.server.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfig {
-//
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/api/**")
-//                        .allowedOrigins("http://localhost:5173")
-//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-//                        .allowedHeaders("*");
-//            }
-//        };
-//    }
-//}
+package com.app.server.config;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+	    CorsConfiguration config = new CorsConfiguration();
+	    config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
+		config.setAllowedMethods(Collections.singletonList("*"));
+		config.setAllowCredentials(true);
+		config.setAllowedHeaders(Collections.singletonList("*"));
+		config.setExposedHeaders(Arrays.asList("Authorization"));
+		config.setMaxAge(3600L);
+
+	    UrlBasedCorsConfigurationSource source =
+	            new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", config);
+	    return source;
+	}
+}
--- server/src/main/java/com/app/server/config/SecurityConfig.java
+++ server/src/main/java/com/app/server/config/SecurityConfig.java
@@ -7,17 +7,14 @@ import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 
 import com.app.server.filters.JWTTokenValidatorFilter;
-
-import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class SecurityConfig {
@@ -34,19 +31,7 @@ public class SecurityConfig {
 			.sessionManagement(
 					sessionConfig -> sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.requiresChannel(rcc -> rcc.anyRequest().requiresInsecure())// requiresSecure
-			.cors(corsConfig -> corsConfig.configurationSource(new CorsConfigurationSource() {
-				@Override
-				public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-					CorsConfiguration config = new CorsConfiguration();
-					config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
-					config.setAllowedMethods(Collections.singletonList("*"));
-					config.setAllowCredentials(true);
-					config.setAllowedHeaders(Collections.singletonList("*"));
-					config.setExposedHeaders(Arrays.asList("Authorization"));
-					config.setMaxAge(3600L);
-					return config;
-				}
-			}))
+			.cors(Customizer.withDefaults())
 			.authorizeHttpRequests((requests) -> requests
 					.requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
 					.requestMatchers("/api/user/register", "/api/auth/login").permitAll()
